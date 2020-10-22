35e0505027e28f6b2ca501160eec771d96da4087
herokunt <48509601+herokunt@users.noreply.github.com>
2020-10-22 23:03:05 +0200

    Removed threading, added additional CLI argument

--- oh_node.py
+++ oh_node.py
@@ -4,18 +4,17 @@
 oh_node.py - do you know how much space do all of those node_modules folders
 take up in your computer? Neither do I! Let's find out...
 
-Usage: node_way.py ROOT_DIR [-v]
+Usage: node_way.py ROOT_DIR [-v verbose][-t top extensions]
 
 TODO: ignore binary type formats (images, executables, etc)
 '''
 
-from concurrent.futures import ThreadPoolExecutor
-from itertools import repeat
 from pathlib import Path
 import argparse
 import logging
 import os
 import re
+
 
 def main():
     args = parse_arguments()
@@ -28,55 +27,68 @@ def main():
     logging.debug('Counting files...')
     files, types, size = count_files(args.root_dir)
 
-    # count total lines of code
-    total_lines = 0
-
     logging.debug('Counting lines of code...')
-    with ThreadPoolExecutor() as executor:
-        results = executor.map(count_lines, files, repeat(types))
+    total_lines = count_lines(files)
 
-        # Calculate and format data
-        total_lines = sum(result for result in results)
-        total_files = len(files)
-        total_size = bytes_2_human_readable(size)
+    # Calculate and format data
+    total_files = len(files)
+    total_size = bytes_2_human_readable(size)
 
-        # Calculate the most common file extensions
-        high_count = sorted(types.values(), reverse=True)[:10]
-        top_10_ext = []
-        for k, v in types.items():
-            if v in high_count and k != 'none':
-                top_10_ext.append((k, v))
-            if len(top_10_ext) == 5:
-                top_10_ext = sorted(top_10_ext, key=lambda x: x[1], reverse=True)
-                break
+    logging.debug(f'Found {total_files} total files with {len(types)} different extensions and a total of {total_size}')
 
-        logging.debug(f'Found {total_files} total files with {len(types)} different extensions and a total of {total_size}')
+    # Print results to terminal output
+    print('oh_node.py - Scan Complete')
+    print(''.rjust(50, '-'))
 
-        # Print results to terminal output
-        print('oh_node.py - Scan Complete')
-        print(''.rjust(50, '-'))
+    # Total files, and most common extension
+    print("Total files".ljust(20, '.') + f"{str(total_files)}".rjust(30, '.'))
 
-        # Total files, and most common extension
-        print("Total files".ljust(20, '.') + f"{str(total_files)}".rjust(30, '.'))
-        for ext in top_10_ext:
-            print(f"  .{ext[0]}".ljust(20) + f"{ext[1]}".rjust(30))
+    # Optionally pass 0 for --top_ext parameter to skip top extensions
+    if args.top_ext:
+        for ext in top_extensions(types, top=args.top_ext):
+            print(f"  {ext[0]}".ljust(20, '.') + f"{ext[1]}".rjust(30, '.'))
 
-        print('Total size'.ljust(20, '.') + f'{total_size}'.rjust(30, '.'))
-        print('Total lines of code'.ljust(20, '.') + f'{str(total_lines)}'.rjust(30, '.'))
+    print('Total size'.ljust(20, '.') + f'{total_size}'.rjust(30, '.'))
+    print('Total lines of code'.ljust(20, '.') + f'{str(total_lines)}'.rjust(30, '.'))
 
 
-def count_lines(file, types):
+def top_extensions(types, top=5):
+    '''Counts, sorts and returns the 5 highest items in the types dictionary'''
+
+    # Error checking...
+    if top > len(types):
+        logging.debug('Showing all extensions...')
+        top = len(types)
+    if top < 0:
+        logging.debug('Showing 5 extensions...')
+        top = 5
+
+    print(f"  Showing {top} most common extensions")
+
+    top_ext = []
+    high_count = sorted(types.values(), reverse=True)[:top+1]
+    for k, v in types.items():
+        if v in high_count and k != 'none':
+            top_ext.append((k, v))
+        if len(top_ext) == top:
+            top_ext = sorted(top_ext, key=lambda x: x[1], reverse=True)
+            break
+    return top_ext
+
+
+def count_lines(files):
     '''Counts the lines of code from a text file'''
 
     lines = 0
-    with open(file, 'r') as f:
-        try:
-            for line in f:
-                lines += 1
-            return lines
-        except UnicodeDecodeError as error:
-            # logging.error(f'Error reading {os.path.basename(file)}: {error}')
-            return 0
+    for file in files:
+        with open(file, 'r') as f:
+            try:
+                for line in f:
+                    lines += 1
+            except UnicodeDecodeError as error:
+                # TODO: error handling for binary formats
+                pass
+    return lines
 
 
 def count_files(root_dir):
@@ -164,6 +176,11 @@ def parse_arguments():
     parser.add_argument('root_dir',
         help='root directory to start looking for node_modules.',
         metavar='root_dir'
+    )
+    parser.add_argument('-t', '--top-ext',
+        help='the number of file extensions to display. Defaults to 5.',
+        type=int,
+        default=5
     )
     parser.add_argument('-v', '--verbose',
         help='prints additional information to terminal output',
