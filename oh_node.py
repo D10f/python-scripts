a8f28731ae488f943ab5138ae3e96843fc3f765d
herokunt <48509601+herokunt@users.noreply.github.com>
2020-10-23 20:45:06 +0200

    Added timing function calls in verbose mode

--- oh_node.py
+++ oh_node.py
@@ -12,6 +12,7 @@ TODO: ignore binary type formats (images, executables, etc)
 from pathlib import Path
 import argparse
 import logging
+import time
 import os
 import re
 
@@ -27,17 +28,17 @@ def main():
     logging.debug('Counting files...')
     files, types, size = count_files(args.root_dir)
 
-    logging.debug('Counting lines of code...')
-    total_lines = count_lines(files)
-
     # Calculate and format data
     total_files = len(files)
     total_size = bytes_2_human_readable(size)
 
-    logging.debug(f'Found {total_files} total files with {len(types)} different extensions and a total of {total_size}')
+    logging.debug('Counting lines of code...')
+    total_lines = count_lines(files)
+
 
     # Print results to terminal output
     print('oh_node.py - Scan Complete')
+    logging.debug(f'Found {total_files} total files with {len(types)} different extensions and a total of {total_size}')
     print(''.rjust(50, '-'))
 
     # Total files, and most common extension
@@ -80,15 +81,23 @@ def count_lines(files):
     '''Counts the lines of code from a text file'''
 
     lines = 0
+    total_lines = 0
+    start = time.time()
     for file in files:
         with open(file, 'r') as f:
             try:
                 for line in f:
                     lines += 1
+                    if lines == 1_000_000:
+                        total_lines += lines
+                        lines = 0
+                        logging.debug(f'{total_lines} lines of code counted in {round(time.time() - start, 2)}s...')
             except UnicodeDecodeError as error:
                 # TODO: error handling for binary formats
                 pass
-    return lines
+
+    logging.debug(f'{total_lines + lines} lines of code counted in {round(time.time() - start, 2)}s...')
+    return total_lines + lines
 
 
 def count_files(root_dir):
@@ -103,6 +112,7 @@ def count_files(root_dir):
     total_files = []
     total_types = {}
 
+    start = time.time()
     for current, dirs, files in os.walk(root_dir):
 
         # ignore files not within a node_modules directory
@@ -130,6 +140,7 @@ def count_files(root_dir):
             # add absolute path to total files
             total_files.append(file)
 
+    logging.debug(f'{len(total_files)} files counted in {round(time.time() - start)}s...')
     return (total_files, total_types, total_size)
 
 
@@ -143,7 +154,7 @@ def bytes_2_human_readable(number_of_bytes):
     if number_of_bytes < 0:
         raise ValueError("!!! number_of_bytes can't be smaller than 0 !!!")
 
-    step_to_greater_unit = 1024.
+    step_to_greater_unit = 1000.
 
     number_of_bytes = float(number_of_bytes)
     unit = 'bytes'
@@ -186,7 +197,6 @@ def parse_arguments():
         help='prints additional information to terminal output',
         action='store_true'
     )
-
     return parser.parse_args()
 
 
