9eff0a3cb4bf890709fee84b37831b933e580cf8
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-02-09 19:29:26 +0100

    add list of all programming related file extensions

--- repo_stats.py
+++ repo_stats.py
@@ -9,10 +9,17 @@ from pathlib import Path
 import subprocess
 import argparse
 import logging
+import json
 import sys
 import os
 
 CURRENT_VERSION = 'v0.0.1'
+
+# DEFEAULT PROGRAMMING LANGUAGE FILE EXTENSIONS
+# https://gist.github.com/ppisarczyk/43962d06686722d26d176fad46879d41
+
+# FILE_EXTENSIONS = json.loads('file_extensions.json')
+FILE_EXTENSIONS = ['.abap', '.asc', '.ash', '.ampl', '.mod', '.g4', '.apl', '.dyalog', '.asp', '.asax', '.ascx', '.ashx', '.asmx', '.aspx', '.axd', '.dats', '.hats', '.sats', '.as', '.adb', '.ada', '.ads', '.agda', '.als', '.cls', '.applescript', '.scpt', '.arc', '.ino', '.aj', '.asm', '.a51', '.inc', '.nasm', '.aug', '.ahk', '.ahkl', '.au3', '.awk', '.auk', '.gawk', '.mawk', '.nawk', '.bat', '.cmd', '.befunge', '.bison', '.bb', '.bb', '.decls', '.bmx', '.bsv', '.boo', '.b', '.bf', '.brs', '.bro', '.c', '.cats', '.h', '.idc', '.w', '.cs', '.cake', '.cshtml', '.csx', '.cpp', '.c++', '.cc', '.cp', '.cxx', '.h', '.h++', '.hh', '.hpp', '.hxx', '.inc', '.inl', '.ipp', '.tcc', '.tpp', '.chs', '.clp', '.cmake', '.cmake.in', '.cob', '.cbl', '.ccp', '.cobol', '.cpy', '.capnp', '.mss', '.ceylon', '.chpl', '.ch', '.ck', '.cirru', '.clw', '.icl', '.dcl', '.click', '.clj', '.boot', '.cl2', '.cljc', '.cljs', '.cljs.hl', '.cljscm', '.cljx', '.hic', '.coffee', '._coffee', '.cake', '.cjsx', '.cson', '.iced', '.cfm', '.cfml', '.cfc', '.lisp', '.asd', '.cl', '.l', '.lsp', '.ny', '.podsl', '.sexp', '.cp', '.cps', '.cl', '.coq', '.v', '.cr', '.feature', '.cu', '.cuh', '.cy', '.pyx', '.pxd', '.pxi', '.d', '.di', '.com', '.dm', '.d', '.dart', '.djs', '.dylan', '.dyl', '.intr', '.lid', '.E', '.ecl', '.eclxml', '.ecl', '.e', '.ex', '.exs', '.elm', '.el', '.emacs', '.emacs.desktop', '.em', '.emberscript', '.erl', '.es', '.escript', '.hrl', '.xrl', '.yrl', '.fs', '.fsi', '.fsx', '.fx', '.flux', '.f90', '.f', '.f03', '.f08', '.f77', '.f95', '.for', '.fpp', '.factor', '.fy', '.fancypack', '.fan', '.fs', '.fth', '.4th', '.f', '.for', '.forth', '.fr', '.frt', '.fs', '.ftl', '.fr', '.gms', '.g', '.gap', '.gd', '.gi', '.tst', '.s', '.ms', '.gd', '.glsl', '.fp', '.frag', '.frg', '.fs', '.fsh', '.fshader', '.geo', '.geom', '.glslv', '.gshader', '.shader', '.vert', '.vrx', '.vsh', '.vshader', '.gml', '.kid', '.ebuild', '.eclass', '.glf', '.gp', '.gnu', '.gnuplot', '.plot', '.plt', '.go', '.golo', '.gs', '.gst', '.gsx', '.vark', '.grace', '.gf', '.groovy', '.grt', '.gtpl', '.gvy', '.gsp', '.hcl', '.tf', '.hlsl', '.fx', '.fxh', '.hlsli', '.hh', '.php', '.hb', '.hs', '.hsc', '.hx', '.hxsl', '.hy', '.bf', '.pro', '.dlm', '.ipf', '.idr', '.lidr', '.ni', '.i7x', '.iss', '.io', '.ik', '.thy', '.ijs', '.flex', '.jflex', '.jq', '.jsx', '.j', '.java', '.jsp', '.js', '._js', '.bones', '.es', '.es6', '.frag', '.gs', '.jake', '.jsb', '.jscad', '.jsfl', '.jsm', '.jss', '.njs', '.pac', '.sjs', '.ssjs', '.sublime-build', '.sublime-commands', '.sublime-completions', '.sublime-keymap', '.sublime-macro', '.sublime-menu', '.sublime-mousemap', '.sublime-project', '.sublime-settings', '.sublime-theme', '.sublime-workspace', '.sublime_metrics', '.sublime_session', '.xsjs', '.xsjslib', '.jl', '.krl', '.sch', '.brd', '.kicad_pcb', '.kt', '.ktm', '.kts', '.lfe', '.ll', '.lol', '.lsl', '.lslp', '.lvproj', '.lasso', '.las', '.lasso8', '.lasso9', '.ldml', '.lean', '.hlean', '.l', '.lex', '.ly', '.ily', '.b', '.m', '.lagda', '.litcoffee', '.lhs', '.ls', '._ls', '.xm', '.x', '.xi', '.lgt', '.logtalk', '.lookml', '.ls', '.lua', '.fcgi', '.nse', '.pd_lua', '.rbxs', '.wlua', '.mumps', '.m', '.m4', '.m4', '.ms', '.mcr', '.muf', '.m', '.mak', '.d', '.mk', '.mkfile', '.mako', '.mao', '.mathematica', '.cdf', '.m', '.ma', '.mt', '.nb', '.nbp', '.wl', '.wlt', '.matlab', '.m', '.maxpat', '.maxhelp', '.maxproj', '.mxt', '.pat', '.m', '.moo', '.metal', '.minid', '.druby', '.duby', '.mir', '.mirah', '.mo', '.mod', '.mms', '.mmk', '.monkey', '.moo', '.moon', '.myt', '.ncl', '.nsi', '.nsh', '.n', '.axs', '.axi', '.axs.erb', '.axi.erb', '.nlogo', '.nl', '.lisp', '.lsp', '.nim', '.nimrod', '.nit', '.nix', '.nu', '.numpy', '.numpyw', '.numsc', '.ml', '.eliom', '.eliomi', '.ml4', '.mli', '.mll', '.mly', '.m', '.h', '.mm', '.j', '.sj', '.omgrofl', '.opa', '.opal', '.cl', '.opencl', '.p', '.cls', '.scad', '.ox', '.oxh', '.oxo', '.oxygene', '.oz', '.pwn', '.inc', '.php', '.aw', '.ctp', '.fcgi', '.inc', '.php3', '.php4', '.php5', '.phps', '.phpt', '.pls', '.pck', '.pkb', '.pks', '.plb', '.plsql', '.sql', '.sql', '.pov', '.inc', '.pan', '.psc', '.parrot', '.pasm', '.pir', '.pas', '.dfm', '.dpr', '.inc', '.lpr', '.pp', '.pl', '.al', '.cgi', '.fcgi', '.perl', '.ph', '.plx', '.pm', '.pod', '.psgi', '.t', '.6pl', '.6pm', '.nqp', '.p6', '.p6l', '.p6m', '.pl', '.pl6', '.pm', '.pm6', '.t', '.l', '.pig', '.pike', '.pmod', '.pogo', '.pony', '.ps1', '.psd1', '.psm1', '.pde', '.pl', '.pro', '.prolog', '.yap', '.spin', '.pp', '.pd', '.pb', '.pbi', '.purs', '.py', '.bzl', '.cgi', '.fcgi', '.gyp', '.lmi', '.pyde', '.pyp', '.pyt', '.pyw', '.rpy', '.tac', '.wsgi', '.xpy', '.qml', '.qbs', '.pro', '.pri', '.r', '.rd', '.rsx', '.rbbas', '.rbfrm', '.rbmnu', '.rbres', '.rbtbar', '.rbuistate', '.rkt', '.rktd', '.rktl', '.scrbl', '.rl', '.reb', '.r', '.r2', '.r3', '.rebol', '.red', '.reds', '.cw', '.rpy', '.rs', '.rsh', '.robot', '.rg', '.rb', '.builder', '.fcgi', '.gemspec', '.god', '.irbrc', '.jbuilder', '.mspec', '.pluginspec', '.podspec', '.rabl', '.rake', '.rbuild', '.rbw', '.rbx', '.ru', '.ruby', '.thor', '.watchr', '.rs', '.rs.in', '.sas', '.smt2', '.smt', '.sqf', '.hqf', '.sql', '.db2', '.sage', '.sagews', '.sls', '.scala', '.sbt', '.sc', '.scm', '.sld', '.sls', '.sps', '.ss', '.sci', '.sce', '.tst', '.self', '.sh', '.bash', '.bats', '.cgi', '.command', '.fcgi', '.ksh', '.sh.in', '.tmux', '.tool', '.zsh', '.sh-session', '.shen', '.sl', '.smali', '.st', '.cs', '.tpl', '.sp', '.inc', '.sma', '.nut', '.stan', '.ML', '.fun', '.sig', '.sml', '.do', '.ado', '.doh', '.ihlp', '.mata', '.matah', '.sthlp', '.sc', '.scd', '.swift', '.sv', '.svh', '.vh', '.txl', '.tcl', '.adp', '.tm', '.tcsh', '.csh', '.t', '.thrift', '.t', '.tu', '.ts', '.tsx', '.upc', '.uno', '.uc', '.ur', '.urs', '.vcl', '.vhdl', '.vhd', '.vhf', '.vhi', '.vho', '.vhs', '.vht', '.vhw', '.vala', '.vapi', '.v', '.veo', '.vim', '.vb', '.bas', '.cls', '.frm', '.frx', '.vba', '.vbhtml', '.vbs', '.volt', '.webidl', '.x10', '.xc', '.xsp-config', '.xsp.metadata', '.xpl', '.xproc', '.xquery', '.xq', '.xql', '.xqm', '.xqy', '.xs', '.xslt', '.xsl', '.xojo_code', '.xojo_menu', '.xojo_report', '.xojo_script', '.xojo_toolbar', '.xojo_window', '.xtend', '.y', '.yacc', '.yy', '.zep', '.zimpl', '.zmpl', '.zpl', '.ec', '.eh', '.fish', '.mu', '.nc', '.ooc', '.wisp', '.prg', '.ch', '.prw']
 
 def main():
   args = parse_arguments()
@@ -52,6 +59,11 @@ def get_local_repository(path_to_repo, branch):
     logger.debug(f'.gitignore file found ({len(ignorefiles)} lines).')
   
   repo_files = select_files(path_to_repo, ignorefiles)
+  
+  result = count_lines_of_code(repo_files)
+
+  print(len(repo_files))
+  print(result)
 
   # count lines of code per extension (js, ts, css, etc)
   # calculate top used languages in %
@@ -60,6 +72,23 @@ def get_local_repository(path_to_repo, branch):
 #   pass
 # def fetch_user_data(username, from_provider = None, omit_repos = None, use_repos = None):
 #   pass
+
+
+def count_lines_of_code(files):
+  """Counts total lines of code from provided list of files and arranges them
+  by file extension in a dictionary.
+  """
+  lines_by_extension = {}
+
+  for file in files:
+    ext = file.suffix
+
+    lines_by_extension.setdefault(ext, 0)
+
+    with open(file, 'rb') as f:
+      lines_by_extension[ext] = lines_by_extension[ext] + sum(1 for _ in f)
+
+  return lines_by_extension
 
 
 def select_files(path_to_repo, ignorefiles):
@@ -72,23 +101,31 @@ def select_files(path_to_repo, ignorefiles):
     # Prune directories and files listed under ignorefiles
     dirnames[:] = [d for d in dirnames if d not in ignorefiles]
 
-    # Exclude files listed under ignorefiles (converting to path-like)
-    result.extend([Path.joinpath(Path(current_dir), Path(f)) for f in filenames])
+    # Exclude files listed under ignorefiles
+    filtered_files = [f for f in filenames if f not in ignorefiles]
+
+    result.extend([Path.joinpath(Path(current_dir), Path(f)) for f in filtered_files])
   
   return result
 
 
 def use_ignorefile(path_to_repo):
-  """Checks for the presence of a .gitignore file in the specified directory"""
+  """Checks for the presence of a .gitignore file in the specified directory
+  and returns it as a list with every entry."""
 
   gitignore_file = Path.joinpath(path_to_repo, '.gitignore')
 
+  # always ignore .git directory
+  result = ['.git']
+
   if not Path.exists(gitignore_file):
-    return []
+    return result
 
   with open(gitignore_file) as file:
-    return [line.rstrip('/\n') for line in file.readlines() if line != '\n']
-
+    result.extend([line.rstrip('/\n') for line in file.readlines() if line != '\n'])
+  
+  return result
+  
 
 def branch_exist(branch_name, path_to_repo):
   """Checks if branch_name exists in the specified git repository"""
